b7055c39fd4eab6daa7a91bd20b6b0a9ff2186d4
Xiaoqi Jiang <Gin@macbookair.lan>
2026-03-22 17:24:17 -0500

    Create SQLAlchemy models for LEGO set database table

---
+++ model.py
@@ -0,0 +1,16 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Float, Boolean
+
+class LegoSet(Base):
+    __table_name__() = "lego_sets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    set_name = Column(String)
+    set_name = Column(String, unique=True)
+    theme = Column(String)
+    purchase_price = Column(Float)
+    quantity = Column(Integer)
+    estimated_market_value = Column(Float, nullable=True)
+    condition = Column(String)
+    is_sealed = Column(Boolean, default=True)
+    notes = Column(String, nullable=True)
